27ddf09a8f3418314f46a413c25815e633803f39
Emgmbakri <mrm507415@gmail.com>
2023-11-30 14:41:48 +0400

    claculations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -16,7 +16,7 @@ def div(a, b):
 
 
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
