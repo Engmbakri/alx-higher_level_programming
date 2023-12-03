ecc7189fa5bdfb73d22cdace9c5d453a26ac0c1a
Emgmbakri <mrm507415@gmail.com>
2023-12-03 19:48:46 +0400

    replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,5 +6,5 @@ def replace_in_list(my_list, idx, element):
     elif idx >= x:
         return my_list
     else:
-        my_list[idx] = enement
+        my_list[idx] = element
         return my_list
