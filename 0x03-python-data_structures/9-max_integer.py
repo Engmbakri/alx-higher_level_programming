ee3a3e5826750280c5964c9f624ad93a7da0726d
Emgmbakri <mrm507415@gmail.com>
2023-12-04 15:48:36 +0400

    max of list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        my_list.sort()
-        max_value = my_list[-1]
+        copy_list = my_list.copy()
+        copy_list.sort()
+        max_value = copy_list[-1]
         print("Max: {:d}".format(max_value))
