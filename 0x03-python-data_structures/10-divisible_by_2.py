bcb7cf2e0d643c71e12949cbce1ae9fd2284d8fe
Emgmbakri <mrm507415@gmail.com>
2023-12-04 18:15:43 +0400

    boolien list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return None
-    else:
-        result = []
-        for i in my_lsit:
-            result.append(i % 2 == 0)
-        return result
+    new_list = []
+    for i in my_lsit:
+        new_list.append(i % 2 == 0)
+    return new_list
