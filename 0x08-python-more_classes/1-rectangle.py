1a65b5cf9249cb06ce45c9ad10cc39b50e07aa86
Emgmbakri <mrm507415@gmail.com>
2024-01-04 20:12:52 +0400

    rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,39 +1,10 @@
 #!/usr/bin/python3
+"""
+define Rectanglr class
+"""
+
 
 class Rectangle:
-    def __init__(self, width=0, height=0):
-        self._width = 0  # Private attribute for width
-        self._height = 0  # Private attribute for height
-        self.width = width  # Set width using the property setter
-        self.height = height  # Set height using the property setter
-
-    @property
-    def width(self):
-        """Getter method to retrieve the width attribute."""
-        return self._width
-
-    @width.setter
-    def width(self, value):
-        """Setter method to set the width attribute."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self._width = value
-
-    @property
-    def height(self):
-        """Getter method to retrieve the height attribute."""
-        return self._height
-
-    @height.setter
-    def height(self, value):
-        """Setter method to set the height attribute."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self._height = valueclass Rectangle:
     def __init__(self, width=0, height=0):
         self._width = 0  # Private attribute for width
         self._height = 0  # Private attribute for height
